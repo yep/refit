cd33086769e811e097998417770922d268b96ef3
chrisp <chrisp@0b6e801c-6c0e-0410-a55b-aaf90caad76e>
2006-03-18 19:02:18 +0000

    Adapted the scripts for refitl.

--- dist/efi/refitl/enable-always.sh
+++ dist/efi/refitl/enable-always.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 DIR="$(pwd)"
-FILE="$DIR/refit.efi"
+FILE="$DIR/refitl.efi"
 
 if [ ! -f "$FILE" ]; then
-  echo "You must run enable.sh from the directory where you put refit.efi!"
+  echo "You must run enable.sh from the directory where you put refitl.efi!"
   exit 1
 fi
 if [ "$(uname -p)" != "i386" ]; then
