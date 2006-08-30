ec27af9963081c4603d9be3a0a9f3de92d09d2cc
chrisp <chrisp@0b6e801c-6c0e-0410-a55b-aaf90caad76e>
2006-08-30 12:56:54 +0000

    Disabled debug options.

--- refit/build.bat
+++ refit/build.bat
@@ -4,7 +4,7 @@ IF ()==(%SDK_BUILD_ENV%) (
   set SDK_BUILD_ENV=bios32
   set SDK_INSTALL_DIR=%CD%\..
   set EFI_APPLICATION_COMPATIBILITY=EFI_APP_110
-  set EFI_DEBUG=YES
+  set EFI_DEBUG=NO
   set PATH=%CD%\..\bin;%PATH%
 )
 nmake -C -S -f refit.mak -nologo %1
