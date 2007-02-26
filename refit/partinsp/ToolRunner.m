1ab0d8f40f55d7fca7305f64b73d8d2c42f195e8
chrisp <chrisp@0b6e801c-6c0e-0410-a55b-aaf90caad76e>
2007-02-26 06:45:34 +0000

    Cleaned up Info.plist and added an icon. Added help menu items (links to the rEFIt webpage). Added some sketchy error handling in the authorization process.

--- refit/partinsp/ToolRunner.m
+++ refit/partinsp/ToolRunner.m
@@ -1,10 +1,38 @@
-//
-//  ToolRunner.m
-//  partshow
-//
-//  Created by Christoph Pfisterer on 26.11.06.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
-//
+/*
+ * partinsp/ToolRunner.m
+ * Helper class to run embedded tool with root privileges
+ *
+ * Copyright (c) 2006-2007 Christoph Pfisterer
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *  * Neither the name of Christoph Pfisterer nor the names of the
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "ToolRunner.h"
 
@@ -109,14 +137,18 @@
             
             if (myStatus == errAuthorizationSuccess) {
                 stdoutHandle = [[NSFileHandle alloc] initWithFileDescriptor:fileno(pipefile)];
+            } else {
+                // TODO: error handling
             }
         }
         //AuthorizationFree(myAuthorizationRef, kAuthorizationFlagDefaults);
     }
     // TODO: error handling
     
-    if (stdoutHandle == nil)
+    if (stdoutHandle == nil) {
+        [callbackObject performSelectorOnMainThread:callbackSelector withObject:nil waitUntilDone:NO];
         return;
+    }
     
 #endif
     
