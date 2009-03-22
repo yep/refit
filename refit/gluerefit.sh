242446ec49dd49efbf3c67f99c140dc1d2a37d1c
chrisp <chrisp@0b6e801c-6c0e-0410-a55b-aaf90caad76e>
2009-03-22 18:03:47 +0000

    Rename tools to lowercase, step 3.

--- refit/gluerefit.sh
+++ refit/gluerefit.sh
@@ -2,7 +2,7 @@
 
 for binary in refit/refit dbounce/dbounce dumpfv/dumpfv dumpprot/dumpprot \
     fsw/fsw_ext2 fsw/fsw_iso9660 fsw/fsw_reiserfs gptsync/gptsync \
-    TextMode/TextMode ; do
+    TextMode/textmode ; do
   ./fatglue.py $(basename $binary).efi \
     ${binary}_bios32.efi ${binary}_em64t.efi
 done
--- refit/gluetools.sh
+++ refit/gluetools.sh
@@ -3,8 +3,8 @@
 cd ../binaries
 mkdir -p fat
 
-../refit/fatglue.py fat/Shell.efi bios32/NShell.efi em64t/NShell.efi
-for tool in DrawBox.efi dhclient.efi ed.efi edit.efi ftp.efi hexdump.efi \
+../refit/fatglue.py fat/shell.efi bios32/NShell.efi em64t/NShell.efi
+for tool in drawbox.efi dhclient.efi ed.efi edit.efi ftp.efi hexdump.efi \
     hostname.efi ifconfig.efi loadarg.efi ping.efi pppd.efi route.efi \
     tcpipv4.efi which.efi ; do
   ../refit/fatglue.py fat/$tool bios32/$tool em64t/$tool
